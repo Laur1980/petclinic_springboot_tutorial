2ed29ad8ca7344e19abb9049de4fe5ef3736bdca
Laur1980 <nicolae_ovidiu_laurentiu@yahoo.com>
2018-08-21 09:43:42 +0300

    initial commit

---
+++ src/main/java/ro/petclinit/tutorial/springboot2/petclinic_springboot_tutorial/PetclinicSpringbootTutorialApplication.java
@@ -0,0 +1,12 @@
+package ro.petclinit.tutorial.springboot2.petclinic_springboot_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclinicSpringbootTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetclinicSpringbootTutorialApplication.class, args);
+	}
+}
---
+++ src/test/java/ro/petclinit/tutorial/springboot2/petclinic_springboot_tutorial/PetclinicSpringbootTutorialApplicationTests.java
@@ -0,0 +1,16 @@
+package ro.petclinit.tutorial.springboot2.petclinic_springboot_tutorial;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PetclinicSpringbootTutorialApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
